806032bdb4536b1b8bd553c07c3f37c1704651b4
tvanoorschot <oorschot98@gmail.com>
2022-10-13 17:28:41 +0200

    security.py aangepast

--- API/security.py
+++ API/security.py
@@ -15,7 +15,10 @@ async def get_api_key(api_key: str = Security(api_key_header), ):
 def auth_gebruiker(api_key):
     gebruiker = get_gebruiker_by_key(api_key)
     if gebruiker is None:
-        raise HTTPException(status_code=403, detail="Gebruiker key niet correct of absent")
+        raise HTTPException(status_code=403, detail="Gebruiker key absent of niet correct")
     else:
-        print("Gebruiker:   " + gebruiker.naam + "   -   Time:    " + str(datetime.now().strftime("%H:%M:%S")))
+        print("Gebruiker:   {naam} ({kamer})   -   Time:    {time}".format(
+            naam=gebruiker.naam,
+            kamer=gebruiker.kamer,
+            time=datetime.now().strftime("%H:%M:%S")))
     return gebruiker
